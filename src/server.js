68c2b070296466f6f953bf7ec08fc6f3ba8fe2d3
194pp <zilvinas111@gmail.com>
2021-10-27 02:39:50 +0300

    Finalized server.js.

--- src/server.js
+++ src/server.js
@@ -10,7 +10,6 @@ express.static('public');
 express.static('public/css');
 mongoose.connect(config.db);
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -22,13 +21,13 @@ const registerRoute = require('./routes/registerRoute');
 const loginRoute = require('./routes/loginRoute');
 const groupsRoute = require('./routes/groupsRoute');
 const checkTokenRoute = require('./routes/checkTokenRoute');
-// TODO: const billsRoute = require('./routes/billsRoute');
+const billsRoute = require('./routes/billsRoute');
 
 app.use('/register', registerRoute);
 app.use('/login', loginRoute);
 app.use('/groups', groupsRoute);
 app.use('/checkToken', checkTokenRoute);
-// TODO: app.use('/bills', billsroute);
+app.use('/bills', billsRoute);
 
 app.listen(port);
 console.log('Listening on port ' + port);
